41e249c1bb3779f6c588c887e5fb87ec783ca52a
Khang000 <khangquach026@gmail.com>
2024-02-04 15:08:39 +0700

    first commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "admin-dashboard",
+    build: {
+        outDir: "build", // Ensure this line is correctly specifying the output directory
+    },
+});
